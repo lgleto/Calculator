c464b1d07e53b95f777a7736b9658ff7a3046188
Lourenço <l.gomes@winwel.com>
2018-03-06 15:58:35 +0000

    - UI Design

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var operand : Double = 0
+    var operation : String = ""
+    
+    var userIsIntheMiddleOfTyping = false
+    
+    var displayValue : Double {
+        get {
+            return Double(labelDisplay.text!)!
+        }
+        set {
+            labelDisplay.text = String(newValue)
+        }
+    }
 
+    @IBOutlet weak var labelDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +36,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func digitPressed(_ sender: UIButton) {
+        let buttonPressed = sender
+        let textCurrentInDisplay = labelDisplay.text
 
+        
+        if userIsIntheMiddleOfTyping {
+            labelDisplay.text = textCurrentInDisplay! + buttonPressed.title(for: .normal)!
+        }else {
+            labelDisplay.text =  buttonPressed.title(for: .normal)!
+            userIsIntheMiddleOfTyping = true
+        }
+        
+        
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        let buttonPressed = sender
+        if userIsIntheMiddleOfTyping {
+        }
+        
+        var result = 0.0
+        
+        switch buttonPressed.title(for: .normal)! {
+        case "+":
+            operand = displayValue
+            operation = buttonPressed.title(for: .normal)!
+        case "=" :
+            result = operand + displayValue
+            displayValue = result
+        default:
+            result = 0
+        }
+        
+        
+        userIsIntheMiddleOfTyping = false
+        
+    }
+    
 }
 
