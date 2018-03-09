030ace439e52742d928749c35beefeded52a5524
Lourenço <l.gomes@winwel.com>
2018-03-09 17:29:19 +0000

    - Model

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var brain = CalculatorBrain()
+    var brain = CalculatorBrain() // Model
     
     var userIsIntheMiddleOfTyping = false
     
